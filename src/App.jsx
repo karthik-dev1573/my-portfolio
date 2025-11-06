ce2dcd5b2af1f2129dd798f63d46cbb5d2382035
karthik-dev1573 <karthikbpcse@gmail.com>
2025-11-06 18:50:17 +0530

    navbar and contact page responsive added also scroll top component created

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Project from "./Pages/Project";
 import Contact from "./Pages/Contact";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../Styles/Nav.css";
 import { navItems } from "../Constants/data";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <nav>
-      <div className="navItems">
-        {navItems.map((navItem, index) => {
-          return (
-            <NavLink key={index} to={navItem.path}>
-              {navItem.name}
-            </NavLink>
-          );
-        })}
+    <nav className="navbar">
+      {/* Hamburger Icon */}
+      <div className="menuIcon" onClick={handleToggle}>
+        {menuOpen ? <FaTimes size={40} /> : <FaBars size={40} />}
+      </div>
+
+      {/* Nav Links */}
+      <div className={`navItems ${menuOpen ? "open" : ""}`}>
+        {navItems.map((navItem, index) => (
+          <NavLink
+            key={index}
+            to={navItem.path}
+            onClick={handleLinkClick}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            {navItem.name}
+          </NavLink>
+        ))}
       </div>
     </nav>
   );
